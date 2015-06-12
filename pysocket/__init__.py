55b14d4a08c174d9c5932c0373a2b0c48be6bcc2
saraivaufc <saraiva.ufc@gmail.com>
2015-06-11 21:35:34 -0300

    audio adicionado

---
+++ pysocket/__init__.py
@@ -0,0 +1,1 @@
+from .pysocket import *
---
+++ pysocket/pysocket.py
@@ -0,0 +1,20 @@
+import sys  #for exit
+import struct
+import time
+
+End = '}'
+
+class Pysocket(object):
+    def __init__(self):
+        pass
+    def recvall(self, sock):
+        total_data=[]
+        data='' 
+        while True: 
+            data=sock.recv(8192)
+            if End in data: 
+                total_data.append(data) 
+                break 
+            total_data.append(data)
+        result=''.join(total_data) 
+        return result
