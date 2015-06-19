793dfaab6193b431174a3ffecb9a63389d870df3
saraivaufc <saraiva.ufc@gmail.com>
2015-06-19 10:28:39 -0300

    Version 2.0

--- tests/testServer.py
+++ tests/testServer.py
@@ -3,7 +3,6 @@ import os.path
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 from server import Server
-import socket, pysocket
-p = pysocket.Pysocket()
-s = Server((p.get_lan_ip(), 50000))
+
+s = Server(('52.10.227.18', 50000))
 s.run()
